a2ae6884242998027fbc61babce7a466a24f16fc
Janet Chuang <janet.chuang@gmail.com>
2013-12-15 22:03:53 -0500

    add validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ActiveRecord::Base
   has_many: posts
   has_many: comments
+
+  validates_presence_of: name
+  validates_presence_of: username
+  validates_presence_of: email
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to: author
   belongs_to: category
   has_many: comments
+
+  validates_presence_of: subject
+  validates_presence_of: body
 end
